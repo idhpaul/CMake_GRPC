b9d85940b955ce5ba3277be16b65c753a32c6158
idhpaul <idhpaul@naver.com>
2021-06-18 17:52:44 +0900

    cpprest test

---
+++ cpprest_client/cpprest_client.h
@@ -0,0 +1,9 @@
+﻿// cpprest_client.h: 대상의 헤더 파일입니다.
+
+#include <cpprest/filestream.h>
+#include <cpprest/http_client.h>
+
+using namespace utility;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency::streams;
